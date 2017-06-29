220f4c87152e119c8ea8bc0087c332feb562346e
15107355 <15107355@qq.com>
2017-06-29 16:46:29 +0800

    2017.6.29

---
+++ app/index/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use app\index\model\User;
+class Index extends Controller
+{
+    public function index()
+    {
+      $res = User::create([
+      		'name'	=> 'imooc',
+      		'password'	=> md5('imooc')
+      	]);
+      dump($res);
+    }
+}
