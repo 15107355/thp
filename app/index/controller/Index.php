8296a702db86c75314a2762c93e49db5ac0f9f36
15107355 <15107355@qq.com>
2017-06-29 17:52:13 +0800

    2017.6.29

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -6,10 +6,15 @@ class Index extends Controller
 {
     public function index()
     {
-      $res = User::create([
-      		'name'	=> 'imooc',
-      		'password'	=> md5('imooc')
-      	]);
-      dump($res);
+      // $res = User::create([
+      // 		'name'	=> 'imooc',
+      // 		'password'	=> md5('imooc')
+      // 	]);	
+      // $user = User::get(1);
+      // $user->name ='17771258';
+      // $res = $user->save();
+    	// $res = User::destroy(3);
+    $res = User::get(4);
+      dump($res->getData());
     }
 }
